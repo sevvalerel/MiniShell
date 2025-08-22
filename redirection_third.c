e9e6273b1c7cbbbfea49ef35a6cf174a8cc41601
Şevval Ayşe Erel <seerel@k1m35s03.42istanbul.com.tr>
2025-08-22 21:59:10 +0300

    ok

--- redirection_third.c
+++ redirection_third.c
@@ -1,97 +1,120 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   redirection_second.c                               :+:      :+:    :+:   */
+/*   redirection_third.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bucolak <bucolak@student.42.fr>            +#+  +:+       +#+        */
+/*   By: seerel <seerel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 15:16:36 by bucolak           #+#    #+#             */
-/*   Updated: 2025/08/22 17:56:42 by bucolak          ###   ########.fr       */
+/*   Updated: 2025/08/22 21:56:45 by seerel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	remove_heredoc(t_general *list)
+static t_arg	**build_heredoc_clean_args(t_arg **args)
 {
+	t_arg	**new_arg;
 	int		i;
 	int		j;
-	t_arg	**new_arg;
-	t_arg **old_arg;
-	if (!list || !list->acces_args || !list->acces_args->args)
-		return ;
-	i = 0;
-	old_arg = list->acces_args->args;
-	while (list->acces_args->args[i])
-	{
-		i++;
-	}
-	new_arg = malloc(sizeof(t_arg *) * (i + 1));
-	if(!new_arg)
-		return ;
+	int		count;
+
+	count = 0;
+	while (args[count])
+		count++;
+	new_arg = malloc(sizeof(t_arg *) * (count + 1));
+	if (!new_arg)
+		return (NULL);
 	i = 0;
 	j = 0;
-	while (list->acces_args->args[i])
+	while (args[i])
 	{
-		if (ft_strcmp(list->acces_args->args[i]->str, "<<") == 0)
+		if (ft_strcmp(args[i]->str, "<<") == 0)
 		{
 			i += 2;
 			continue ;
 		}
-		new_arg[j++] = list->acces_args->args[i++];
+		new_arg[j++] = args[i++];
 	}
 	new_arg[j] = NULL;
-	list->acces_args->args = new_arg;
+	return (new_arg);
+}
+static void	free_heredoc_args(t_arg **args)
+{
+	int	i;
+
 	i = 0;
-	while(old_arg[i])
+	while (args[i])
 	{
-		if(ft_strcmp(old_arg[i]->str,"<<") == 0)
+		if (ft_strcmp(args[i]->str, "<<") == 0)
 		{
-			free(old_arg[i]->str);
-			free(old_arg[i]);
+			free(args[i]->str);
+			free(args[i]);
 			i++;
-			if(old_arg[i])
+			if (args[i])
 			{
-				free(old_arg[i]->str);
-				free(old_arg[i]);
+				free(args[i]->str);
+				free(args[i]);
 				i++;
 			}
-			continue;
+			continue ;
 		}
 		i++;
 	}
-	free(old_arg);
+	free(args);
 }
+void	remove_heredoc(t_general *list)
+{
+	t_arg	**old_args;
+	t_arg	**new_args;
 
+	if (!list || !list->acces_args || !list->acces_args->args)
+		return ;
+	old_args = list->acces_args->args;
+	new_args = build_heredoc_clean_args(old_args);
+	if (!new_args)
+		return ;
+	list->acces_args->args = new_args;
+	free_heredoc_args(old_args);
+}
+static int	count_heredoc(t_general *list)
+{
+	int	i;
+	int	count;
+
+	if (!list || !list->acces_args || !list->acces_args->args)
+		return (0);
+	i = 0;
+	count = 0;
+	while (list->acces_args->args[i])
+	{
+		if (ft_strcmp(list->acces_args->args[i]->str, "<<") == 0)
+			count++;
+		i++;
+	}
+	return (count);
+}
 void	fill_limiter(t_general *list)
 {
 	int	i;
 	int	j;
-	int	c;
+	int	heredoc_count;
 
-	i = 0;
-	j = 0;
-	c = 0;
 	if (!list || !list->acces_args || !list->acces_args->args)
 		return ;
-	while(list->acces_args->args[i])
-	{
-		if(ft_strcmp(list->acces_args->args[i]->str, "<<") == 0)
-			c++;
-		i++;
-	}
-	list->limiter = malloc(sizeof(char *)*(c +1));
+	heredoc_count = count_heredoc(list);
+	list->limiter = malloc(sizeof(char *) * (heredoc_count + 1));
+	if (!list->limiter)
+		return ;
 	i = 0;
+	j = 0;
 	while (list->acces_args->args[i])
 	{
-		if (ft_strcmp(list->acces_args->args[i]->str, "<<") == 0)
+		if (ft_strcmp(list->acces_args->args[i]->str, "<<") == 0
+			&& list->acces_args->args[i + 1])
 		{
-			if (list->acces_args->args[i + 1])
-			{
-				list->limiter[j] = ft_strdup(list->acces_args->args[i
-						+ 1]->str);
-				j++;
-			}
+			list->limiter[j] = ft_strdup(list->acces_args->args[i + 1]->str);
+			j++;
 		}
 		i++;
 	}
@@ -109,126 +132,116 @@ void	free_heredoc(t_full *full)
 	else
 	{
 		cleanup(temp_full);
-		if(temp_full->pipe_blocks)
+		if (temp_full->pipe_blocks)
 			free_pipe_blocks(temp_full->pipe_blocks);
 		exit(130);
 	}
 }
-void signal_handler_heredoc(int signo)
+void	signal_handler_heredoc(int signo)
 {
 	if (signo == SIGINT)
 	{
 		write(1, "\n", 1);
-		// rl_on_new_line();
-        // rl_replace_line("", 0);
-		// rl_redisplay();
 		signal_ec = 1;
 		free_heredoc(NULL);
 		exit(130);
 	}
 }
 
+static void	cleanup_heredoc_exit(t_full *full, int fd)
+{
+	close(fd);
+	cleanup(full);
+	free_pipe_blocks(full->pipe_blocks);
+	exit(0);
+}
+
+static void	read_heredoc_input(t_full *full, int fd_write, char *limiter)
+{
+	char	*line;
+
+	free_heredoc(full);
+	close(fd_write - 1);
+	signal(SIGINT, signal_handler_heredoc);
+	signal(SIGQUIT, SIG_IGN);
+	while (1)
+	{
+		line = readline("heredoc > ");
+		if (!line)
+			cleanup_heredoc_exit(full, fd_write);
+		if (ft_strcmp(line, limiter) == 0)
+		{
+			free(line);
+			cleanup_heredoc_exit(full, fd_write);
+		}
+		ft_putstr_fd(line, fd_write);
+		ft_putstr_fd("\n", fd_write);
+		free(line);
+	}
+}
+static int	handle_heredoc_process(t_general *tmp, t_full *full, int *j,
+		int *status)
+{
+	int		fd[2];
+	pid_t	pid;
+
+	pipe(fd);
+	signal(SIGINT, SIG_IGN);
+	pid = fork();
+	if (pid == 0)
+		read_heredoc_input(full, fd[1], tmp->limiter[*j]);
+	close(fd[1]);
+	waitpid(pid, status, 0);
+	signal(SIGINT, handle_signal);
+	signal(SIGQUIT, SIG_IGN);
+	if (WIFEXITED(*status) && WEXITSTATUS(*status) == 130)
+	{
+		tmp->a = 1;
+		close(fd[0]);
+		remove_heredoc(tmp);
+		tmp->heredoc_fd = -1;
+		tmp->dqm = 130;
+		return (1);
+	}
+	tmp->heredoc_fd = fd[0];
+	return (0);
+}
+static int	process_heredocs(t_general *tmp, t_full *full)
+{
+	int	i;
+	int	j;
+	int	status;
+
+	i = 0;
+	j = 0;
+	status = 0;
+	while (tmp->acces_args->args[i])
+	{
+		if (ft_strcmp(tmp->acces_args->args[i]->str, "<<") == 0)
+		{
+			tmp->flag_heredoc = 1;
+			if (!tmp->acces_args->args[i + 1])
+			{
+				ft_putstr_fd("bash: syntax error near unexpected token `newline'\n",
+					2);
+				return (1); // Hata varsa 1 döndür
+			}
+			if (handle_heredoc_process(tmp, full, &j, &status))
+				return (1);
+			j++;
+			i += 2;
+		}
+		else
+			i++;
+	}
+	remove_heredoc(tmp);
+	return (0);
+}
+
 void	handle_heredoc(t_general *list, t_full *full)
 {
-	int		i;
-	int		fd[2];
-	int j;
-	int s = 0;
-	char	*line;
-	t_general *tmp;
-	(void)full;
-	tmp = list;
-		fill_limiter(tmp);
-		s++;
-		i = 0;
-		j = 0;
-		while (tmp->acces_args->args[i])
-		{
-			if (ft_strcmp(tmp->acces_args->args[i]->str, "<<") == 0)
-			{
-				tmp->flag_heredoc = 1;
-				if (!tmp->acces_args->args[i + 1])
-				{
-					ft_putstr_fd("bash: syntax error near unexpected token `newline'\n", 2);
-					return ;
-				}
-				pipe(fd);
-				signal(SIGINT, SIG_IGN);
-				pid_t pid = fork();
-				if(pid == 0)
-				{
-					free_heredoc(full);
-					close(fd[0]);
-					// if (full && full->pipe && full->pipe->fd)
-					// {
-					// 	int ci = 0;
-					// 	while (ci < full->pipe->count - 1)
-					// 	{
-					// 		close(full->pipe->fd[ci][0]);
-					// 		close(full->pipe->fd[ci][1]);
-					// 		ci++;
-					// 	}
-					// }
-					signal(SIGINT, signal_handler_heredoc);
-        			signal(SIGQUIT, SIG_IGN);
-					while (1)
-					{
-						line = readline("heredoc > ");
-						if (!line) // Ctrl+D durumu
-                    	{
-                    	    close(fd[1]);
-							cleanup(full);
-							free_pipe_blocks(full->pipe_blocks);
-                    	    exit(0);
-                    	}
-	
-                    	if (ft_strcmp(line, tmp->limiter[j]) == 0) // Limiter bulundu
-                    	{
-                    	    free(line);
-                    	    close(fd[1]);
-							cleanup(full);
-							free_pipe_blocks(full->pipe_blocks);
-                    	    exit(0);
-                    	}
-						ft_putstr_fd(line, fd[1]);
-						ft_putstr_fd("\n", fd[1]);
-						free(line);
-					}
-					close(fd[1]);
-					j++;
-					if (!tmp->limiter[j])
-					{
-						tmp->heredoc_fd = dup(fd[0]);
-					}
-					i += 2;
-					continue ;
-				}
-    			int status;
-				j++;
-    			close(fd[1]);
-    			waitpid(pid, &status, 0);
-				signal(SIGINT, handle_signal);
-				signal(SIGQUIT, SIG_IGN);
-    			if (WIFEXITED(status) && WEXITSTATUS(status) == 130)
-    			{
-					tmp->a = 1;
-    			    close(fd[0]);
-					remove_heredoc(tmp);
-					tmp->heredoc_fd = -1;
-    			    // write(1, "\n", 1);
-					//free_split(full->pipe_blocks->limiter);
-        			//signal(SIGINT, handle_signal);
-					tmp->dqm = 130;
-					return ;
-    			}
-    			else
-    			{
-    			    // heredoc başarılı
-					//signal(SIGINT, handle_signal);
-    			    tmp->heredoc_fd = fd[0];
-    			}
-			}
-			i++;
-		}
-		remove_heredoc(tmp);
+	if (!list || !list->acces_args || !list->acces_args->args)
+		return ;
+	fill_limiter(list);
+	process_heredocs(list, full);
 }
